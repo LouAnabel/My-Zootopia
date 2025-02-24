2eae8746cca56ba74def06f73c24e9c6c320bf29
louisedebatin <loudebatin@gmail.com>
2025-02-24 11:57:44 +0100

    added def main()

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,15 +1,6 @@
 import json
 
-with open("animals_template.html", 'r', encoding='utf-8') as html_file:
-    html_content = html_file.read()
-
-def load_data(file_path):
-  with open(file_path, "r") as file:
-    return json.load(file)
-animals_data = load_data('animals_data.json')
-
-
-def serialize_animal(animal_obj):
+def serialize_animal(animal):
     output = ' '
     output += "<li class='cards__item'>\n"
     output += f"<div class='card__title'>{animal['name']}</div><br/>\n"
@@ -24,14 +15,24 @@ def serialize_animal(animal_obj):
     output += "</li>"
     return output
 
+def main():
+    with open("animals_template.html", 'r', encoding='utf-8') as html_file:
+        html_content = html_file.read()
 
-output = ''
-for animal in animals_data:
-    output += serialize_animal(animal)
+    def load_data(file_path):
+        with open(file_path, "r") as file:
+            return json.load(file)
 
-html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
+    animals_data = load_data('animals_data.json')
 
-with open("animals.html", 'w') as new_file:
-    new_file.write(html_content)
+    output = ''
+    for animal in animals_data:
+        output += serialize_animal(animal)
 
+    html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
 
+    with open("animals.html", 'w') as new_file:
+        new_file.write(html_content)
+
+if __name__ == "__main__":
+    main()
