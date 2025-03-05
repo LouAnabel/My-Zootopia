045653f0188d34ffd92529e71b049d6c09d08ae5
louisedebatin <loudebatin@gmail.com>
2025-03-05 12:46:51 +0100

    Error handling with try and except

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,16 +5,33 @@ animal that will be added to the json and displayed on the website"""
 def serialize_animal(animal):
     output = ' '
     output += "<li class='cards__item'>\n"
-    output += f"<div class='card__title'>{animal.get('name', 'Unknown')}</div><br/>\n"
+
+    try:
+        output += f"<div class='card__title'>{animal['name']}</div><br/>\n"
+    except KeyError:
+        output += "<div class='card__title'>Unknown</div><br/>\n"
     output += "<div class='card__text'>\n"
-    output += "<ul class='info_block'\n>"
-    output += f"<li class='diet'><strong>Diet:</strong> {animal.get('characteristics', 'Unknown')['diet']}<br/>\n"
-    output += f"<li class='location'><strong>Location:</strong> {' and '.join(animal.get('locations', 'Unknown'))}<br/>\n"
-    if "type" in animal['characteristics']:
+    output += "<ul class='info_block'>\n"
+
+    try:
+        output += f"<li class='diet'><strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
+    except KeyError:
+        output += "<li class='diet'><strong>Diet:</strong> Unknown<br/>\n"
+    try:
+        location_text = ' and '.join(animal['locations'])
+    except KeyError:
+        location_text = "Unknown"
+    except TypeError:
+        location_text = "Unknown"
+    output += f"<li class='location'><strong>Location:</strong> {location_text}<br/>\n"
+    try:
         output += f"<li class='type'><strong>Type:</strong> {animal['characteristics']['type']}\n"
-    output += "</ul>"
-    output += "</div>"
-    output += "</li>"
+    except KeyError:
+        pass
+    output += "</ul>\n"
+    output += "</div>\n"
+    output += "</li>\n"
+
     return output
 
 def main():
